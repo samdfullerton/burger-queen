765aafb60568f67f5f0fbea254ac4d46105a1559
Sam Fullerton <Samdfullerton@gmail.com>
2020-11-30 13:13:38 -0600

    config folder update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,4 @@
 INSERT INTO burgers (name, devoured) VALUES ('Whopper', False);
 INSERT INTO burgers (name, devoured) VALUES ('Double Quarter Pounder', False);
+INSERT INTO burgers (name, devoured) VALUES ('Classic Deluxe', False);
+INSERT INTO burgers (name, devoured) VALUES ('Meaty Meatless Double Cheese Vegan Burger', False);
